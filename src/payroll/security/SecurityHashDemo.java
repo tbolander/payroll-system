2844f792b6d91695f8cd6574fc8beb1a17054708
Tyler Bolander <tbolander2@ivytech.edu>
2026-05-12 22:32:13 -0500

    Removed hash demo print statement

--- src/payroll/security/SecurityHashDemo.java
+++ src/payroll/security/SecurityHashDemo.java
@@ -3,10 +3,6 @@ package payroll.security;
 public class SecurityHashDemo {
     public static void main(String[] args) {
         String demoPassword = "ABCpayroll2026!";
-        String hashedPassword = PasswordHasher.hashMd5(demoPassword);
-
-        System.out.println("Security Module Hashing Demo");
-        System.out.println("Password: " + demoPassword);
-        System.out.println("MD5 Hash: " + hashedPassword);
+        PasswordHasher.hashMd5(demoPassword);
     }
 }
